bc85145e8e631032369f1845a47fa84a7a247b35
Daniela <Daniela Gomes@LAPTOP-194QUC87.(none)>
2024-04-04 17:06:29 +0100

    quickstart

---
+++ genealogicaltreeapp/src/test/java/com/genealogicaltreeapp/genealogicaltreeapp/repositories/FamilyInformationRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.genealogicaltreeapp.genealogicaltreeapp.repositories;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+
+//o que quer isto dizer?
+@ExtendWith(SpringExtension.class)
+public class FamilyInformationRepositoryTest {
+
+    private FamilyInformationRepository familyInformationRepository;
+
+}
