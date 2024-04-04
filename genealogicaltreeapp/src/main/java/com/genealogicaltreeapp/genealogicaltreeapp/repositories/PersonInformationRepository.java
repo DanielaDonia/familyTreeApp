28bc25282b87b9dd99b4967bc074a70b689242b5
Daniela <Daniela Gomes@LAPTOP-194QUC87.(none)>
2024-04-04 17:14:40 +0100

    quickstart

--- genealogicaltreeapp/src/main/java/com/genealogicaltreeapp/genealogicaltreeapp/repositories/PersonInformationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.genealogicaltreeapp.genealogicaltreeapp.repositories;
-
-import com.genealogicaltreeapp.genealogicaltreeapp.domain.PersonInformation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonInformationRepository extends JpaRepository<PersonInformation, Long> {
-}
